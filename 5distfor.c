aaa294df571e4910e67c410ead80d4feeb99b45e
Sani104 <76279283+Sani104@users.noreply.github.com>
2021-03-20 16:12:42 +0530

    Arthmetic Operators

---
+++ 5distfor.c
@@ -0,0 +1,7 @@
+int main()
+d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));//apply the distance formula
+	printf("distance betweem these two points is %f \n" ,x1,x2,y1,y2,d);//printf funtion calling to print the distance between the two points 
+
+	return 0;//returning statements 
+	return 0;//retur statement 
+}//end of main body functions
---
+++ Sumnum.c
@@ -0,0 +1,10 @@
+//A basic C Program to calculate sum of two numbers.
+Name:Sanidhya Sharma
+#include<stdio.h>
+int main(){
+	int number1, number2, sum;
+	scanf("%d %d", &number1, &number2); //Scanning values entered by user.
+	sum=number1+number2; //Processing the sum operation.
+	printf("Sum of 2 numbers= %d", sum); //Printing the output.
+	return 0;
+} 
