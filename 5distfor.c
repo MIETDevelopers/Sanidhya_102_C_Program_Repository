84c9da165b6f9ffc36667d86d5ae81faf0e9600a
Sani104 <76279283+Sani104@users.noreply.github.com>
2021-03-22 09:58:30 +0530

    Distance Between two points

--- 5distfor.c
+++ 5distfor.c
@@ -1,7 +1,19 @@
-int main()
-d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));//apply the distance formula
-	printf("distance betweem these two points is %f \n" ,x1,x2,y1,y2,d);//printf funtion calling to print the distance between the two points 
-
-	return 0;//returning statements 
-	return 0;//retur statement 
-}//end of main body functions
+#include<stdio.h>  
+#include<math.h>  
+  
+int main()  
+{  
+    float x1, y1, x2, y2, distance;  
+  
+    printf("Enter point 1 (x1, y1)\n");  
+    scanf("%f%f", &x1, &y1);  
+  
+    printf("Enter point 2 (x2, y2)\n");  
+    scanf("%f%f", &x2, &y2);  
+  
+    distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) );  
+  
+    printf("Distance between (%0.2f, %0.2f) and (%0.2f, %0.2f) is %0.2f\n", x1, y1, x2, y2, distance);  
+  
+    return 0;  
+}  
