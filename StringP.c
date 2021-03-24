74da4dc4640bde45dfb2e1757b8e21738762feed
Sani104 <76279283+Sani104@users.noreply.github.com>
2021-03-24 08:53:35 +0530

    Program to read the string in pointers loaction and give it as output

---
+++ StringP.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(void) {
+  char name[] = "Sanidhya Sharma";
+
+  printf("%c \n", *name);     	// Output will be I
+  printf("%c \n", *(name+3));	// Output will be A
+  printf("%c \n", *(name+8));   // Output will be R
+
+  char *namePtr;
+
+  namePtr = name;
+  printf("%c \n", *namePtr);     // Output will be I
+  printf("%c \n", *(namePtr+2)); // Output will be H
+  printf("%c \n", *(namePtr+7)); // Output will be E
+} 
