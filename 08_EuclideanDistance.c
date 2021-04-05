a9806f68e9209ef7725e3c0810db762499b504aa
Sani104 <76279283+Sani104@users.noreply.github.com>
2021-04-05 19:54:38 +0530

    Update 08_EuclideanDistance.c

--- 08_EuclideanDistance.c
+++ 08_EuclideanDistance.c
@@ -1,4 +1,4 @@
-//AUther-Sanidhya Sharma
+//Author-Sanidhya Sharma
 //A C Program to calculate distance between two points.
 #include<stdio.h> //Including Standard input & output header file from library. 
 #include<math.h>  //Including maths header file from library/
